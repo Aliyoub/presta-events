5062f9f80ec3948fdd66ddc40937480b8c345ae9
presta <event@gmail.com>
2024-09-29 16:15:29 +0200

    update to escape ...

--- app/anniversaires/page.tsx
+++ app/anniversaires/page.tsx
@@ -13,41 +13,41 @@ export default function Home() {
       className="container-style"
     >
       <p className="p-style">
-        L’organisation d’un anniversaire demande une bonne dose de planification
+        L\’organisation d\’un anniversaire demande une bonne dose de planification
         pour garantir une célébration réussie. Tout commence par la définition
         du type de fête souhaitée. S'agit-il d'une grande fête réunissant
-        famille et amis ou d'un événement plus intime avec quelques proches ?
+        famille et amis ou d'un événement plus intime avec quelques proches?
         Les goûts de la personne fêtée sont également un point essentiel à
         prendre en compte pour personnaliser l'événement : une soirée dansante,
         un dîner raffiné ou une sortie à thème peuvent être envisagés en
         fonction de ses préférences. Ce choix de base influence le reste de
-        l’organisation.
+        l\’organisation.
       </p>
       <p className="p-style">
         Le choix du lieu est crucial pour accueillir tous les invités dans les
         meilleures conditions. Certains préféreront organiser la fête à
-        domicile, surtout si l’ambiance est conviviale et familiale. D’autres
+        domicile, surtout si l\’ambiance est conviviale et familiale. D\’autres
         opteront pour une salle de réception, un restaurant ou même un espace en
         plein air, selon la saison. Le lieu doit être adapté au nombre
-        d’invités, mais aussi à l’atmosphère que l’on souhaite créer. Une salle
-        élégante conviendra à une fête formelle, tandis qu’un jardin ou une
+        d\’invités, mais aussi à l\’atmosphère que l\’on souhaite créer. Une salle
+        élégante conviendra à une fête formelle, tandis qu\’un jardin ou une
         terrasse sera idéal pour une célébration décontractée et festive.
       </p>
 
       <p className="p-style">
-        Le thème de l’anniversaire est souvent un élément phare de
-        l’organisation. Il permet de donner une direction claire à la décoration
+        Le thème de l\’anniversaire est souvent un élément phare de
+        l\’organisation. Il permet de donner une direction claire à la décoration
         et aux activités prévues. Que ce soit un thème basé sur une décennie
         spécifique (années 80, années 90), sur une passion de la personne
         (cinéma, voyage, super-héros) ou simplement sur une couleur ou une
         ambiance (tropicale, chic, bohème), le thème doit refléter la
-        personnalité de l’hôte. Une fois le thème choisi, les invitations, les
+        personnalité de l\’hôte. Une fois le thème choisi, les invitations, les
         tenues, et même le gâteau peuvent être adaptés pour créer une cohérence
         visuelle et thématique.
       </p>
 
       <p className="p-style">
-        L’un des aspects les plus importants d’une fête d’anniversaire est la
+        L\’un des aspects les plus importants d\’une fête d\’anniversaire est la
         gestion des invitations. La liste des invités doit être soigneusement
         élaborée en tenant compte des relations proches et des affinités de la
         personne célébrée. Les invitations peuvent être envoyées sous forme
@@ -58,13 +58,13 @@ export default function Home() {
         correctement.
       </p>
       <p className="p-style">
-        La décoration joue un rôle central dans l’ambiance générale de la fête.
+        La décoration joue un rôle central dans l\’ambiance générale de la fête.
         Que ce soit pour un anniversaire enfantin avec des ballons multicolores
         et des personnages de dessins animés, ou pour un anniversaire adulte
         avec des fleurs, des lumières tamisées et des éléments plus
-        sophistiqués, la décoration doit capter l’esprit de la fête. Les tables,
-        le buffet, et même l’espace où se déroulera la soirée dansante, si
-        prévu, doivent être soignés. On peut aussi personnaliser l’espace avec
+        sophistiqués, la décoration doit capter l\’esprit de la fête. Les tables,
+        le buffet, et même l\’espace où se déroulera la soirée dansante, si
+        prévu, doivent être soignés. On peut aussi personnaliser l\’espace avec
         des photos ou des objets qui rappellent des moments marquants de la vie
         de la personne fêtée.
       </p>
@@ -72,21 +72,21 @@ export default function Home() {
         Les animations sont un élément clé pour garantir que les invités passent
         un bon moment. Selon le type d'anniversaire, cela peut aller des jeux
         traditionnels pour enfants, à des karaokés, des jeux de société ou des
-        photobooths pour des invités plus âgés. Il est aussi possible d’engager
+        photobooths pour des invités plus âgés. Il est aussi possible d\’engager
         des professionnels, comme un DJ, un groupe de musique ou un magicien,
-        pour animer la soirée et créer des moments mémorables. L’important est
-        de s’assurer que les animations sont adaptées au public et qu’elles
-        reflètent l’esprit de la fête.
+        pour animer la soirée et créer des moments mémorables. L\’important est
+        de s\’assurer que les animations sont adaptées au public et qu\’elles
+        reflètent l\’esprit de la fête.
       </p>
       <p className="p-style">
-        Enfin, le choix du gâteau d’anniversaire est une étape incontournable.
-        Il ne s’agit pas seulement d’un dessert, mais d’un élément central de la
-        célébration. Le gâteau doit être à la hauteur de l’événement, tant par
+        Enfin, le choix du gâteau d\’anniversaire est une étape incontournable.
+        Il ne s\’agit pas seulement d\’un dessert, mais d\’un élément central de la
+        célébration. Le gâteau doit être à la hauteur de l\’événement, tant par
         son goût que par son apparence. Il peut être personnalisé en fonction
         des goûts de la personne fêtée, que ce soit en termes de saveurs
         (chocolat, fruits, vanille) ou de design (avec des motifs, des
-        figurines, ou même en forme d’objet symbolique). Le moment du gâteau,
-        souvent accompagné d'une chanson d’anniversaire, est l'un des plus
+        figurines, ou même en forme d\’objet symbolique). Le moment du gâteau,
+        souvent accompagné d'une chanson d\’anniversaire, est l'un des plus
         attendus, et doit être un véritable point fort de la soirée.
       </p>
       <style jsx>{`
--- app/mariages/page.tsx
+++ app/mariages/page.tsx
@@ -11,18 +11,18 @@ export default function Home() {
       className="container-style"
     >
       <p className="p-style">
-        L’organisation d’un mariage est une étape cruciale dans la préparation
-        de ce grand événement. Cela commence par la définition d’une vision
+        L\’organisation d\’un mariage est une étape cruciale dans la préparation
+        de ce grand événement. Cela commence par la définition d\’une vision
         commune entre les futurs mariés. Avant même de plonger dans les détails,
         il est essentiel de discuter du style souhaité : un mariage intime ou
         grandiose, en plein air ou dans une salle somptueuse, avec un thème
         spécifique ou plutôt classique. Ces choix guideront toutes les décisions
         à venir et permettront de créer une cérémonie à l'image des futurs
-        mariés, pleine d’émotions et de moments inoubliables.
+        mariés, pleine d\’émotions et de moments inoubliables.
       </p>
       <p className="p-style">
-        Le budget est également un élément central dans l’organisation d’un
-        mariage. Établir une enveloppe financière réaliste permet d’éviter les
+        Le budget est également un élément central dans l\’organisation d\’un
+        mariage. Établir une enveloppe financière réaliste permet d\’éviter les
         mauvaises surprises et de mieux répartir les dépenses. Les postes de
         dépense les plus importants incluent souvent la location de la salle, le
         traiteur, la robe de mariée, les costumes, et bien sûr, la photographie
@@ -37,26 +37,26 @@ export default function Home() {
         planification. En fonction de la saison, les disponibilités des lieux
         peuvent varier, tout comme les tarifs. Certains couples choisissent une
         date symbolique, comme l'anniversaire de leur rencontre, tandis que
-        d’autres privilégient une période où tous les proches seront
+        d\’autres privilégient une période où tous les proches seront
         disponibles. Une fois la date fixée, il devient plus facile de réserver
-        les prestataires et d’envoyer les invitations dans les délais adéquats.
+        les prestataires et d\’envoyer les invitations dans les délais adéquats.
       </p>
       <p className="p-style">
         La sélection des prestataires est une tâche clé qui demande du temps et
         de la recherche. Du traiteur à la décoration florale, en passant par le
         DJ ou le groupe de musique, chacun joue un rôle essentiel dans le bon
         déroulement de la journée. Il est souvent conseillé de rencontrer
-        plusieurs prestataires avant de faire un choix définitif, pour s’assurer
-        qu’ils partagent la vision du mariage et qu’ils sauront la concrétiser
-        avec professionnalisme. Les témoignages d’autres mariés peuvent
+        plusieurs prestataires avant de faire un choix définitif, pour s\’assurer
+        qu\’ils partagent la vision du mariage et qu\’ils sauront la concrétiser
+        avec professionnalisme. Les témoignages d\’autres mariés peuvent
         également être précieux pour faire un choix éclairé.
       </p>
       <p className="p-style">
         La gestion des invités est un autre défi de taille. Élaborer la liste
-        d’invités peut être source de discussions, car il faut souvent jongler
+        d\’invités peut être source de discussions, car il faut souvent jongler
         entre les envies de partager ce moment avec un maximum de proches et les
-        contraintes de place ou de budget. Ensuite vient la création et l’envoi
-        des faire-part, qui doivent être envoyés bien à l’avance pour que chacun
+        contraintes de place ou de budget. Ensuite vient la création et l\’envoi
+        des faire-part, qui doivent être envoyés bien à l\’avance pour que chacun
         puisse réserver la date. Le suivi des réponses est également crucial
         pour anticiper le nombre de convives et ajuster les réservations de
         repas, d'hébergement, etc.
@@ -68,18 +68,18 @@ export default function Home() {
         de chandeliers et de nappes en satin, le décor doit refléter le style et
         les goûts des mariés. Il est souvent utile de faire appel à un
         décorateur professionnel pour coordonner les différents éléments et
-        s'assurer que tout est harmonieux, depuis les centres de table jusqu’au
+        s'assurer que tout est harmonieux, depuis les centres de table jusqu\’au
         choix des couleurs.
       </p>
       <p className="p-style">
         Enfin, l'organisation d'un mariage ne serait pas complète sans la
-        préparation de la cérémonie elle-même. Qu’elle soit religieuse, civile
+        préparation de la cérémonie elle-même. Qu\’elle soit religieuse, civile
         ou symbolique, il est important de réfléchir au déroulement de cette
-        étape clé, aux vœux échangés, et à l’ambiance qui en découlera. Certains
+        étape clé, aux vœux échangés, et à l\’ambiance qui en découlera. Certains
         couples choisissent des rituels symboliques ou des lectures qui leur
-        tiennent à cœur, tandis que d’autres optent pour une cérémonie plus
-        traditionnelle. Dans tous les cas, l’émotion sera au rendez-vous,
-        faisant de ce moment l’un des plus marquants de la journée.
+        tiennent à cœur, tandis que d\’autres optent pour une cérémonie plus
+        traditionnelle. Dans tous les cas, l\’émotion sera au rendez-vous,
+        faisant de ce moment l\’un des plus marquants de la journée.
       </p>
       <style jsx>{`
         .container-style {
