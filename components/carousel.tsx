3475a02c075d13cb1a5d67bbac57dd9022f129d5
presta <event@gmail.com>
2024-09-30 18:22:43 +0200

    Gastronomie, Photographes et Vidéastes, Décorateurset Scénographes added

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -22,7 +22,7 @@ export default function Carousel() {
           // style={{ height: 500 }}
           className="embla__slide flex items-center justify-center slide_container_height"
         >
-          <h2 className="slide_description">Des moments inoubliables !!!</h2>
+          <h2 className="slide_description">Des moments inoubliables...</h2>
           <Image
             src="/presta-events-images/paillettes.jpg"
             alt="paillettes"
@@ -37,7 +37,7 @@ export default function Carousel() {
           // style={{ height: 500 }}
           className="embla__slide flex items-center justify-center slide_container_height"
         >
-          <h2 className="slide_description">La joie de vivre !!!</h2>
+          <h2 className="slide_description">La joie de vivre...</h2>
           <Image
             src="/presta-events-images/ballons-anniversaire.jpg"
             alt="ballons anniversaire"
